45ee660ba5ab6cfccf56e9767f8962425ec57f97
BaileyDev01 <HEM23004070@heartofyorkshire.ac.uk>
2025-01-23 16:25:08 +0000

    Change 3
    Implemented a sign system, checkpoint system and death trigger

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -31,6 +31,11 @@ public class GameManager : MonoBehaviour
     {
         GameTimer();
         Collectables();
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 
     void GameTimer()
